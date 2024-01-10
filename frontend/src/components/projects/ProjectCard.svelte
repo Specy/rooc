81255c84c89063cf6ea78f128376b181911299e9
Specy <specy.dev@gmail.com>
2024-01-10 13:30:19 +0100

    increased stack size

--- frontend/src/components/projects/ProjectCard.svelte
+++ frontend/src/components/projects/ProjectCard.svelte
@@ -12,7 +12,7 @@
 	import Button from '$cmp/inputs/Button.svelte';
 	const dispatcher = createEventDispatcher<{
 		onUpdate: { project: Project };
-		onDownoad: { project: Project };
+		onDownload: { project: Project };
 		onDelete: { project: Project };
 	}>();
 	let name = project.name;
@@ -24,50 +24,57 @@
 </script>
 
 <Card border="secondary" style="position:relative; height: 10rem;" radius="0.4rem">
-	<div class="blurred-underlay">
-		{project.content}
-	</div>
-	<Column padding="0.5rem; margin-bottom: 0.4rem;" style="flex:1;">
-		<div class="title" contenteditable bind:textContent={name} spellcheck="false" on:blur={save} />
-		<div
-			class="description"
-			contenteditable
-			bind:textContent={description}
-			spellcheck="false"
-			on:blur={save}
-		/>
-		<div class="project-dates">
-			<span>Last update </span> <span>{sago(new Date(project.updatedAt))}</span>
-			<span>Created </span> <span>{sago(new Date(project.createdAt))}</span>
-		</div>
+	<pre class="blurred-underlay">{project.content}</pre>
+	<Column style="z-index: 2;">
+		<Column padding="0.5rem; margin-bottom: 0.4rem;" style="flex:1;">
+			<div
+				class="title"
+				contenteditable
+				bind:textContent={name}
+				spellcheck="false"
+				on:blur={save}
+			/>
+			<div
+				class="description"
+				contenteditable
+				bind:textContent={description}
+				spellcheck="false"
+				on:blur={save}
+			/>
+			<div class="project-dates">
+				<span>Last update </span> <span>{sago(new Date(project.updatedAt))}</span>
+				<span>Created </span> <span>{sago(new Date(project.createdAt))}</span>
+			</div>
+		</Column>
+		<Row
+			background="secondary"
+			style="border-radius:0 0 0.4rem 0.4rem;"
+			padding="0.3rem"
+			justify="end"
+			gap="0.5rem"
+		>
+			<Button on:click={() => dispatcher('onDownload', { project })} hasIcon>
+				<Download />
+			</Button>
+			<Button on:click={() => dispatcher('onDelete', { project })} hoverColor="danger" hasIcon>
+				<Delete />
+			</Button>
+			<ButtonLink href="/projects/{project.id}" title="Open" border="secondary">Open</ButtonLink>
+		</Row>
 	</Column>
-	<Row
-		background="secondary"
-		style="border-radius:0 0 0.4rem 0.4rem;"
-		padding="0.3rem"
-		justify="end"
-		gap="0.5rem"
-	>
-		<Button on:click={() => dispatcher('onDownoad', { project })} hasIcon>
-			<Download />
-		</Button>
-		<Button on:click={() => dispatcher('onDelete', { project })} hoverColor="danger" hasIcon>
-			<Delete />
-		</Button>
-		<ButtonLink href="/projects/{project.id}" title="Open" border="secondary">Open</ButtonLink>
-	</Row>
 </Card>
 
 <style lang="scss">
 	.blurred-underlay {
 		position: absolute;
+		margin: 1rem;
 		top: 0;
 		left: 0;
-		width: 100%;
+		width: calc(100% - 2rem);
 		height: 100%;
-		opacity: 0.5;
-		filter: blur(0.5rem);
-		z-index: -1;
+		opacity: 0.2;
+		filter: blur(0.05rem);
+		overflow: hidden;
 	}
 	.project-dates {
 		display: grid;
@@ -79,8 +86,6 @@
 		color: var(--hint);
 	}
 	.description {
-		display: flex;
-		flex: 1;
 		padding: 0.5rem;
 		background-color: transparent;
 		transition: all 0.2s;
