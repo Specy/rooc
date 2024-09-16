0537993d37252e0f36e93de7592bb1df75326d19
Specy <specy.dev@gmail.com>
2024-09-16 21:06:09 +0200

    Improve landing page

--- frontend/src/routes/projects/+page.svelte
+++ frontend/src/routes/projects/+page.svelte
@@ -87,7 +87,7 @@
     {#if !$projectStore.initialized}
         <h2>Loading...</h2>
     {/if}
-    {#if !$projectStore.projects.length && !$projectStore.initialized}
+    {#if !$projectStore.projects.length && $projectStore.initialized}
         <p style="width:100%; text-align:center;">No projects yet, create one!</p>
     {/if}
 </Page>
